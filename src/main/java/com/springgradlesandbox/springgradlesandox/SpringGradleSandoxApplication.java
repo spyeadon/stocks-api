6e03cd92ea2577be53d2341f9b4bf3356d0a38e2
Soren Yeadon <syeadon@vmware.com>
2021-11-17 14:11:39 -0600

    initial commit

---
+++ src/main/java/com/springgradlesandbox/springgradlesandox/SpringGradleSandoxApplication.java
@@ -0,0 +1,13 @@
+package com.springgradlesandbox.springgradlesandox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleSandoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleSandoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springgradlesandbox/springgradlesandox/SpringGradleSandoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springgradlesandbox.springgradlesandox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleSandoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
